8378292953fa6782d44138813d6e9f9a49ad1b42
Jignesh Patil <patiljignesh@gmail.com>
2022-08-21 23:56:35 +0200

    Test Add.

--- app/src/test/java/com/anushka/circlecalc/MyCalcTest.kt
+++ app/src/test/java/com/anushka/circlecalc/MyCalcTest.kt
@@ -1,5 +1,23 @@
 package com.anushka.circlecalc
 
-import org.junit.Assert.*
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
 
-class MyCalcTest
+class MyCalcTest {
+
+    private lateinit var myCalc: MyCalc
+
+    @Before
+    fun setUp() {
+        myCalc = MyCalc()
+    }
+
+    //SubjectUnderTest_actionOrInput_resultState
+    // Add test Anotation
+    @Test
+    fun calculateCircumference_radiusGiven_returnsCorrectResult(){
+        val result = myCalc.calculateCircumference(2.1)
+        assertThat(result).isEqualTo(13.188)
+    }
+}
